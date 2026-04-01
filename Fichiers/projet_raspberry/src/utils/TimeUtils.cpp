6c87e09e08d9e1590879d940789e7f8a4438c9f0
Ethann-4 <ethann2906@gmail.com>
2026-04-01 14:43:11 +0200

    Implement date and time utility functions
    Added functions to get the current date and time as strings.

--- Fichiers/projet_raspberry/src/utils/TimeUtils.cpp
+++ Fichiers/projet_raspberry/src/utils/TimeUtils.cpp
@@ -1,1 +1,22 @@
+#include "TimeUtils.h"
+#include <ctime>
+#include <iomanip>
+#include <sstream>
 
+std::string TimeUtils::nowDate() {
+    std::time_t t = std::time(nullptr);
+    std::tm *tm = std::localtime(&t);
+
+    std::ostringstream oss;
+    oss << std::put_time(tm, "%Y-%m-%d");
+    return oss.str();
+}
+
+std::string TimeUtils::nowTime() {
+    std::time_t t = std::time(nullptr);
+    std::tm *tm = std::localtime(&t);
+
+    std::ostringstream oss;
+    oss << std::put_time(tm, "%H:%M:%S");
+    return oss.str();
+}
