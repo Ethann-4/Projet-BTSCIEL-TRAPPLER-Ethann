f4acdf1b69d6a586b116c6b2d96e54418dcc8077
Ethann-4 <ethann2906@gmail.com>
2026-04-01 14:07:40 +0200

    Add ERFrame class and ERData struct definition

--- Fichiers/projet_raspberry/src/er/ERFrame.h
+++ Fichiers/projet_raspberry/src/er/ERFrame.h
@@ -1,1 +1,19 @@
 
+#pragma once
+#include <string>
+
+struct ERData {
+    int water;
+    float energy;
+    bool valid;
+};
+
+class ERFrame {
+public:
+    static ERData parse(const std::string& frame);
+};
+
+
+
+
+
