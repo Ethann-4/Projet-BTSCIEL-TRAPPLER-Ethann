ca45825f0be2380a480321c412f5a334a782e2a3
Ethann-4 <ethann2906@gmail.com>
2026-04-01 14:07:56 +0200

    Update ERFrame.cpp

--- Fichiers/projet_raspberry/src/er/ERFrame.cpp
+++ Fichiers/projet_raspberry/src/er/ERFrame.cpp
@@ -1,1 +1,14 @@
+#include "ERFrame.h"
+#include <sstream>
+
+ERData ERFrame::parse(const std::string& frame) {
+    ERData data{0, 0.0f, false};
+
+    if (frame.find("WATER=") == std::string::npos) return data;
+
+    sscanf(frame.c_str(), "WATER=%d;ENERGY=%f", &data.water, &data.energy);
+    data.valid = true;
+    return data;
+}
+
 
