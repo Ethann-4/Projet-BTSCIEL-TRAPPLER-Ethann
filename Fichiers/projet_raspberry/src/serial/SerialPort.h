cb09cea2af29d1c49287bd6940f4d34947e9a68b
Ethann-4 <ethann2906@gmail.com>
2026-04-01 14:40:07 +0200

    Update SerialPort.h

--- Fichiers/projet_raspberry/src/serial/SerialPort.h
+++ Fichiers/projet_raspberry/src/serial/SerialPort.h
@@ -1,1 +1,19 @@
+#pragma once
+#include <string>
 
+class SerialPort {
+public:
+    SerialPort();
+    ~SerialPort();
+
+    bool openPort(const std::string& device, int baudrate);
+    void closePort();
+
+    bool writeLine(const std::string& line);
+    std::string readLine();
+
+    bool isOpen() const;
+
+private:
+    int fd;
+};
