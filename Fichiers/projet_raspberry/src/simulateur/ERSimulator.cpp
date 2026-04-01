8f24b53fbe223b6d43798396792bf2a02e4c2792
Ethann-4 <ethann2906@gmail.com>
2026-04-01 14:41:32 +0200

    Implement frame generation in ERSimulator
    Add frame generation for water and energy values

--- Fichiers/projet_raspberry/src/simulateur/ERSimulator.cpp
+++ Fichiers/projet_raspberry/src/simulateur/ERSimulator.cpp
@@ -1,1 +1,9 @@
+#include "ERSimulator.h"
+#include <cstdlib>
 
+std::string ERSimulator::generateFrame() {
+    int water = rand() % 5000;
+    float energy = (rand() % 1000) / 10.0;
+    return "WATER=" + std::to_string(water) +
+           ";ENERGY=" + std::to_string(energy) + "\n";
+}
