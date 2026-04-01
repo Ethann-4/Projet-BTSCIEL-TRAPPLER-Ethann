bb459c78aa4760aaacd6f3231dda966aa0737bf2
Ethann-4 <ethann2906@gmail.com>
2026-04-01 14:12:38 +0200

    Update HttpClient.h

--- Fichiers/projet_raspberry/src/network/HttpClient.h
+++ Fichiers/projet_raspberry/src/network/HttpClient.h
@@ -1,1 +1,13 @@
+#pragma once
+#include "data/Mesure.h"
+#include <string>
+
+class HttpClient {
+public:
+    static bool sendMesure(const Mesure& m);
+    static std::string getLastError();
+
+private:
+    static std::string lastError;
+};
 
