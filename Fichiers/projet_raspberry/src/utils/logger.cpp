177213e7e1bb24b89be9129a9a3b8136ce200fc7
Ethann-4 <ethann2906@gmail.com>
2026-04-01 14:42:42 +0200

    Update logger.cpp

--- Fichiers/projet_raspberry/src/utils/logger.cpp
+++ Fichiers/projet_raspberry/src/utils/logger.cpp
@@ -1,1 +1,20 @@
+#include "logger.h"
+#include <fstream>
+#include <ctime>
 
+std::string getTime() {
+    time_t now = time(nullptr);
+    char buf[64];
+    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", localtime(&now));
+    return std::string(buf);
+}
+
+void logSystem(const std::string& message) {
+    std::ofstream log("logs/system.log", std::ios::app);
+    log << getTime() << " | " << message << std::endl;
+}
+
+void logError(const std::string& message) {
+    std::ofstream log("logs/error.log", std::ios::app);
+    log << getTime() << " | " << message << std::endl;
+}
