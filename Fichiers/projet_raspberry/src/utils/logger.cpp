44e99089f61041fe4f272869e5a22bcee2136b85
Ethann-4 <ethann2906@gmail.com>
2026-06-15 19:12:19 +0200

    Update logger.cpp

--- Fichiers/projet_raspberry/src/utils/logger.cpp
+++ Fichiers/projet_raspberry/src/utils/logger.cpp
@@ -3,10 +3,10 @@
 #include <ctime>
 
 std::string getTime() {
-    time_t now = time(nullptr);
-    char buf[64];
-    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", localtime(&now));
-    return std::string(buf);
+    time_t now = time(nullptr); // Récupère le temps actuel brut du Raspberry Pi.
+    char buf[64];               // Prépare un tableau de caractères pour stocker le texte final.
+    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", localtime(&now)); // Traduit le temps brut en heure locale et le formate proprement
+    return std::string(buf);    // Transforme le tableau de texte en std::string et le renvoie.
 }
 
 void logSystem(const std::string& message) {
